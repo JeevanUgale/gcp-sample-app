76e480154aeb32877eb02a30e21ad4a8363ddc43
JeevanUgale <112151811+JeevanUgale@users.noreply.github.com>
2024-11-13 16:06:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+const buildNumber = process.env.BUILD_NUMBER || 'V1';
+
 app.get('/', (req, res) => { 
-  res.send('Hello World from GKE!'); 
+  res.send('Hello World from GKE! V${buildNumber}'); 
 });
 app.listen(8080, () => { 
   console.log('App running on port 8080');
