dbca8aa6424a99e55a439c733b4220fdfed781a5
JeevanUgale <112151811+JeevanUgale@users.noreply.github.com>
2024-11-19 12:53:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => { 
-  res.send('Hello World from GKE with new version!'); 
+  res.send('Hello World from the latest version of the application !'); 
 });
 app.listen(8080, () => { 
   console.log('App running on port 8080');
